594c8b72e8f0ce5edc584f461a4efa0e004c8356
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-24 13:03:19 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
+  require 'pry'
   # write your code here!
+  pigeon_hash = {}
+  
+  data.each do |key, hash|
+    keys = hash.values.flatten.uniq
+    hash.each do |type, arr|
+      keys.each do |pigeon|
+        type_arr = []
+         if arr.include?(pigeon)
+          #binding.pry
+          if pigeon_hash.has_key?(pigeon)
+            
+            pigeon_hash[pigeon][key] = type_arr << type.to_s
+          else 
+            type_arr << type.to_s
+            pigeon_hash[pigeon] = {key => type_arr}
+            #binding.pry
+          end
+        end
+      end
+    end
+  end
+  pigeon_hash
 end
