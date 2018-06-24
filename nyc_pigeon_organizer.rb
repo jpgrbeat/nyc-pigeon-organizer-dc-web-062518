014580eaad704b1a01329916344d3f56ea61b5e0
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-24 14:06:57 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,26 +2,28 @@ def nyc_pigeon_organizer(data)
   require 'pry'
   # write your code here!
   pigeon_hash = {}
-  
-  data.each do |key, hash|
-    keys = hash.values.flatten.uniq
-    hash.each do |type, arr|
-      keys.each do |pigeon|
-        type_arr = []
-         if arr.include?(pigeon)
-          #binding.pry
-          if pigeon_hash.has_key?(pigeon)
-            type_arr << type.to_s
-            pigeon_hash[pigeon][key] = type_arr
-            binding.pry
-          else 
-            type_arr << type.to_s
-            pigeon_hash[pigeon] = {key => type_arr}
-            #binding.pry
-          end
-        end
-      end
-    end
+  data.each_with_object({}) do |(key, h), result|
+    binding.pry
+  end
+  # data.each do |key, hash|
+  #   keys = hash.values.flatten.uniq
+  #   hash.each do |type, arr|
+  #     keys.each do |pigeon|
+  #       type_arr = []
+  #       if arr.include?(pigeon)
+  #         #binding.pry
+  #         if pigeon_hash.has_key?(pigeon)
+  #           type_arr << type.to_s
+  #           pigeon_hash[pigeon][key] = type_arr
+  #           binding.pry
+  #         else 
+  #           type_arr << type.to_s
+  #           pigeon_hash[pigeon] = {key => type_arr}
+  #           #binding.pry
+  #         end
+  #       end
+  #     end
+  #   end
   end
   pigeon_hash
 end
